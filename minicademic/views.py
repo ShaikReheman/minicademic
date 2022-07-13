42aa4b9a7dfe06bfd86ea92cc979d7b1b360f173
AngelD-F <angeldfuenmayor21@gmail.com>
2022-07-13 01:11:30 -0400

    trouble

--- minicademic/views.py
+++ minicademic/views.py
@@ -16,14 +16,17 @@ def update_repository(request: HttpRequest):
         if is_valid_signature(x_hub_signature, request.body, os.getenv('WEBHOOK_TOKEN')):
             git.Repo(settings.BASE_DIR).remotes.origin.pull()
             return HttpResponse('Web server updated!')
-    return HttpResponse('Sorry, but you are not allowed!')
+    return HttpResponseNotAllowed('Sorry, but you are not allowed!')
 
 
 def is_valid_signature(x_hub_signature, data, private_key):
     # x_hub_signature and data are from the webhook payload
     # private key is your webhook secret
-    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
-    algorithm = hashlib.__dict__.get(hash_algorithm)
-    encoded_key = bytes(private_key, 'latin-1')
-    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    try:
+        hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+        algorithm = hashlib.__dict__.get(hash_algorithm)
+        encoded_key = bytes(private_key, 'latin-1')
+        mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    except:
+        return False
     return hmac.compare_digest(mac.hexdigest(), github_signature)
