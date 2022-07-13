282a3d0fdd71a144c61a9f677be31a4cb1157a63
Angel <32184965+AngelD-F@users.noreply.github.com>
2022-07-12 20:21:56 -0400

    Update settings.py

--- minicademic/settings.py
+++ minicademic/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'es-VE'
 
 TIME_ZONE = 'America/Caracas'
 
